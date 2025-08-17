223ae60c2a073098eec8e7efd5060d9618b5b698
Ryan <ryanjennett@gmail.com>
2025-08-16 23:57:04 -0600

    Implement vertical only movement on paddle.

--- scenes/paddle/Paddle.cs
+++ scenes/paddle/Paddle.cs
@@ -1,9 +1,11 @@
 using Godot;
 using System;
 
-public partial class Paddle : Node2D
+public partial class Paddle : CharacterBody2D
 {
     PackedScene ball;
+
+    private int _speed = 300;
 
     public override void _Ready()
     {
@@ -15,11 +17,26 @@ public partial class Paddle : Node2D
         // Add new ball scene instance to node tree
         AddChild(ballInstance);
 
+        // Start game with ball attached to player paddle, player1 if two players
         // Set position for new ballInstance
-        ballInstance.Position = Position + new Vector2(6,0);
+        ballInstance.Position = Position + new Vector2(6, 0);
     }
 
-    // Start game with ball attached to player paddle, player1 if two players
-    // Launch ball on click
+    public override void _PhysicsProcess(double delta)
+    {
+        GetInput();
+        MoveAndCollide(Velocity * (float)delta);
+    }
+
+
+    public void GetInput()
+    {
+        float inputY = Input.GetAxis("up_w", "down_s");
+
+        // Provide 0 X and calculated Y for only vertical movement
+        Velocity = new Vector2(0f, inputY * _speed);
+    }
+
+    // Launch ball on input
     // Ball direction based on paddle collision position
 }
