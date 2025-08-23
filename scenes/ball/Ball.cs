58c396b4b8e8f8bcc6c596f8d54c86f824f559e8
Ryan <ryanjennett@gmail.com>
2025-08-23 14:43:34 -0600

    AN20-20 Remove unused physics code.

--- scenes/ball/Ball.cs
+++ scenes/ball/Ball.cs
@@ -10,29 +10,8 @@ public partial class Ball : RigidBody2D
     {
     }
 
-
     public override void _IntegrateForces(PhysicsDirectBodyState2D state)
     {
-        // Detect collision with walls and paddles
-        // for (int i = 0; i < state.GetContactCount(); i++)
-        // {
-        //     // Reference the current Vector2 velocity
-        //     Vector2 localVelocity = state.GetContactLocalVelocityAtPosition(0);
-
-        //     // Case for each local normal, alter velocity
-        //     Vector2 normal = state.GetContactLocalNormal(0);
-        //     switch (normal)
-        //     {
-        //         case var value when value == Vector2.Up:
-        //             Vector2 bounce = new Vector2(localVelocity.X, localVelocity.Y * -1);
-        //             state.LinearVelocity = new Vector2(0, 0);
-        //             ApplyCentralImpulse(bounce);
-        //             break;
-        //     }
-
-        // }
-
-        // Apply bounce based on collision normal
     }
 
     public void Launch()
