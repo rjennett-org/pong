1acc54fc50adba534f5fbde1209623a06d461825
Ryan <ryanjennett@gmail.com>
2025-08-23 14:41:53 -0600

    AN20-20 Commit commented code for physics processing that will not actually be used due to game engine handling.

--- scenes/ball/Ball.cs
+++ scenes/ball/Ball.cs
@@ -4,7 +4,7 @@ using System;
 public partial class Ball : RigidBody2D
 {
 
-    Vector2 newForce = new Vector2(2, 2);
+    Vector2 newForce = new Vector2(40, 40);
 
     public override void _Ready()
     {
@@ -14,6 +14,24 @@ public partial class Ball : RigidBody2D
     public override void _IntegrateForces(PhysicsDirectBodyState2D state)
     {
         // Detect collision with walls and paddles
+        // for (int i = 0; i < state.GetContactCount(); i++)
+        // {
+        //     // Reference the current Vector2 velocity
+        //     Vector2 localVelocity = state.GetContactLocalVelocityAtPosition(0);
+
+        //     // Case for each local normal, alter velocity
+        //     Vector2 normal = state.GetContactLocalNormal(0);
+        //     switch (normal)
+        //     {
+        //         case var value when value == Vector2.Up:
+        //             Vector2 bounce = new Vector2(localVelocity.X, localVelocity.Y * -1);
+        //             state.LinearVelocity = new Vector2(0, 0);
+        //             ApplyCentralImpulse(bounce);
+        //             break;
+        //     }
+
+        // }
+
         // Apply bounce based on collision normal
     }
 
